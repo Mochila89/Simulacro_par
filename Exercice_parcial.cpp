add153c74cc78ae6f2d37320ccfcf80cc795781c
Mochila89 <Alexeivalley@gamil.com>
2025-06-07 08:50:36 -0600

    Simulacro

---
+++ Exercice_parcial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a, b, suma;
+    cout << "suma: " << endl;
+    cin >> a;
+    cin >> b;
+
+    suma = a+b;
+    cout << "tu respuesta: " << suma;
+    return 0;
+}
